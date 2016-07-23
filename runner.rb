9132c21ed1ec4c3dba6b3383e585e3d09630fec7
Tony Narisi <tonynarisi@gmail.com>
2016-07-22 20:41:39 -0400

    Make intiial introduction ascii art

--- runner.rb
+++ runner.rb
@@ -5,7 +5,10 @@ require_relative 'model/card'
 require_relative 'model/deck'
 require_relative 'view/display'
 require_relative 'view/clear_screen'
+require_relative 'view/ascii_art'
 
-ClearScreen::reset_screen
-DeckSelector::display_choices
-GameController.new(DeckSelector::evaluate_user_choice).play_round
+# ClearScreen::reset_screen
+# DeckSelector::display_choices
+# GameController.new(DeckSelector::evaluate_user_choice).play_round
+
+AsciiArt::display_start_screen
